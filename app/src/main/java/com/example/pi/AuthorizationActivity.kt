8089254d09c39cf4f397664bf9699a0448c1075e
maxitool <Parfenov.max24@yandex.ru>
2023-09-20 15:32:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pi/AuthorizationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pi
+
+import android.app.Activity
+import android.os.Bundle
+
+class AuthorizationActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
